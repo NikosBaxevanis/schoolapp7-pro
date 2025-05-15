5a6ca73f053c0ca7724c9dc975379672e6e82973
NikosBaxevanis <baxemb@gmail.com>
2025-05-15 19:15:36 +0300

    feat: Validators Mappers and Controllers

---
+++ src/main/java/gr/aueb/cf/schoolapp/rest/TeacherRestController.java
@@ -0,0 +1,36 @@
+package gr.aueb.cf.schoolapp.rest;
+
+import gr.aueb.cf.schoolapp.core.exceptions.EntityInvalidArgumentException;
+import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolapp.service.ITeacherService;
+import gr.aueb.cf.schoolapp.validator.ValidatorHelper;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@ApplicationScoped
+@RequiredArgsConstructor(onConstructor_ = @__(@Inject))
+@Path("/teachers")
+public class TeacherRestController {
+
+
+    private final ITeacherService teacherService;
+
+//    @Inject
+//    public TeacherRestController(ITeacherService teacherService) {
+//        this.teacherService = teacherService;
+//    }
+
+    public Response addTeacher(TeacherInsertDTO insertDTO, @Context UriInfo uriInfo) throws  EntityInvalidArgumentException{
+        List<String > errors = ValidatorHelper.validateDTO(insertDTO);
+        if (!errors.isEmpty()) {
+            throw  new EntityInvalidArgumentException("teacher", String.join("\n",errors));
+        }
+    }
+}
